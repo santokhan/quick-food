f6596f0aa9ba7585697ba78bd31e2eb6714c7c01
Santo Khan <santok54@gmail.com>
2021-05-26 22:52:31 +0600

    Update app-left-bar.jsx

--- client/src/component/app-left-bar/app-left-bar.jsx
+++ client/src/component/app-left-bar/app-left-bar.jsx
@@ -5,9 +5,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export default function AppLeftBar() {
     return (
         <div className="app-left-bar d-flex flex-column align-items-centerbg-light bg-gradient">
-            <div className="logo-container border-bottom py-lg-3 px-lg-4">
+            <div className="logo-container border-bottom py-lg-3 px-lg-4 bg-danger">
                 <img src="https://img-premium.flaticon.com/png/512/2922/2922037.png?token=exp=1622035407~hmac=9309c9382228014e64ed0c56d0087a89"
-                    alt="logo" width="40" />
+                    alt="logo" width="36" />
             </div>
             <div className="py-2"></div>
             <a href="/client/public/index.html" className="text-decoration-none p-lg-3 my-lg-3 mx-lg-4 icon-container text-light bg-danger bg-gradient"><i className="fas fa-home"></i></a>
