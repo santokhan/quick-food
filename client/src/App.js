b28bbcc5ce70fec14afc18cc0057e0afb6d86ce0
Santo Khan <santok54@gmail.com>
2021-05-26 18:17:30 +0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import AppLeftBar from './component/app-left-bar/app-left-bar';
+import AppMiddle from './component/app-middle/app-middle';
+import AppRightBar from './component/app-right-bar/app-right-bar';
 
 function App() {
   return (
     <div>
       <AppLeftBar />
-      <AppLeftBar />
-      <AppLeftBar />
+      <AppMiddle />
+      <AppRightBar />
     </div>
   );
 }
