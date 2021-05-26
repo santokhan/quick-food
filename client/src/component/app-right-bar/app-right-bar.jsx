92766c4695cb5292757917e21fe6172e6f45d744
Santo Khan <santok54@gmail.com>
2021-05-27 03:00:20 +0600

    Update app-right-bar.jsx

--- client/src/component/app-right-bar/app-right-bar.jsx
+++ client/src/component/app-right-bar/app-right-bar.jsx
@@ -3,22 +3,20 @@ import './app-right-bar.css';
 
 function MyOrder(name, price, amount) {
     return (
-        <div className="row">
+        <div className="row mx-2 rounded order-cart-round">
             <div className="col d-flex justify-content-between align-items-center p-4 bg-light my-2">
                 <div className="icon-container bg-danger d-flex justify-content-center align-items-center bg-gradient">
                     <i className="fas fa-envelope"></i>
                 </div>
                 <div>x</div>
                 <div>{amount}</div>
-                <div>
-                    <div><strong className="text-secondary">{name}</strong></div>
-                    <div>
-                        <strong>{price}</strong>
-                    </div>
+                <div className='d-flex flex-column align-items-center'>
+                    <div><h6 className="text-secondary">{name}</h6></div>
+                    <div><h6>{price}</h6></div>
                 </div>
-                <div>
+                <button className='btn btn-light px-1 py-0'>
                     <i className="fas fa-trash-alt text-danger"></i>
-                </div>
+                </button>
             </div>
         </div>
     );
@@ -29,89 +27,38 @@ export default function AppRightBar() {
         <div className="app-right-bar">
             <div className="container-fluid">
                 <div className="row">
+                    <div className="col d-flex align-items-center">
+                        <div>
+                            <img src="https://thumbs.dreamstime.com/b/businessman-avatar-image-beard-hairstyle-male-profile-vector-illustration-178545831.jpg"
+                                alt="profile" width="50" className="rounded-circle m-3" />
+                        </div>
+                        <div>
+                            <h5 className='m-0'>Santo Khan</h5>
+                            <i className='fas fa-star text-danger'></i>
+                            <i className='fas fa-star text-danger'></i>
+                            <i className='fas fa-star text-danger'></i>
+                            <i className='fas fa-star text-light'></i>
+                            <i className='fas fa-star text-light'></i>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
                     <div className="col d-flex justify-content-between align-items-center px-4">
-                        <h4 className="m-0">Your order</h4>
-                        <p className="m-0">Edit</p>
+                        <h5 className="my-lg-3">Your order</h5>
+                        <button className='btn btn-outline-danger px-1 py-0'>
+                            <span className='px-1'>Edit</span><i className='fas fa-edit px-1'></i>
+                        </button>
                     </div>
                 </div>
                 {MyOrder("Chicken", "15.35$", 1)}
 
                 {MyOrder("Egg", "15.35$", 2)}
 
-                {MyOrder("Meat", "15.35$", 2)}
+                {MyOrder("Meat", "15.35$", 1)}
 
-                <div className="row">
-                    <div className="col d-flex justify-content-between align-items-center p-4 bg-light my-2">
-                        <div className="icon-container bg-danger d-flex justify-content-center align-items-center bg-gradient">
-                            <i className="fas fa-envelope"></i>
-                        </div>
-                        <div>x</div>
-                        <div>1</div>
-                        <div>
-                            <div><strong className="text-secondary">Chicken</strong></div>
-                            <div>
-                                <strong>15.35$</strong>
-                            </div>
-                        </div>
-                        <div>
-                            <i className="fas fa-trash-alt text-danger"></i>
-                        </div>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col d-flex justify-content-between align-items-center p-4 bg-light my-2">
-                        <div className="icon-container bg-danger d-flex justify-content-center align-items-center bg-gradient">
-                            <i className="fas fa-envelope"></i>
-                        </div>
-                        <div>x</div>
-                        <div>1</div>
-                        <div>
-                            <div><strong className="text-secondary">Chicken</strong></div>
-                            <div>
-                                <strong>15.35$</strong>
-                            </div>
-                        </div>
-                        <div>
-                            <i className="fas fa-trash-alt text-danger"></i>
-                        </div>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col d-flex justify-content-between align-items-center p-4 bg-light my-2">
-                        <div className="icon-container bg-danger d-flex justify-content-center align-items-center bg-gradient">
-                            <i className="fas fa-envelope"></i>
-                        </div>
-                        <div>x</div>
-                        <div>1</div>
-                        <div>
-                            <div><strong className="text-secondary">Chicken</strong></div>
-                            <div>
-                                <strong>15.35$</strong>
-                            </div>
-                        </div>
-                        <div>
-                            <i className="fas fa-trash-alt text-danger"></i>
-                        </div>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col d-flex justify-content-between align-items-center p-4 bg-light my-2">
-                        <div className="icon-container bg-danger d-flex justify-content-center align-items-center bg-gradient">
-                            <i className="fas fa-envelope"></i>
-                        </div>
-                        <div>x</div>
-                        <div>1</div>
-                        <div>
-                            <div><strong className="text-secondary">Chicken</strong></div>
-                            <div>
-                                <strong>15.35$</strong>
-                            </div>
-                        </div>
-                        <div>
-                            <i className="fas fa-trash-alt text-danger"></i>
-                        </div>
-                    </div>
-                </div>
+                {MyOrder("Meat", "15.35$", 3)}
+
+                {MyOrder("Meat", "15.35$", 2)}
             </div>
             <div className="d-flex justify-content-center">
                 <a href="/client/public/index.html" className="btn btn-danger bg-gradient px-5 m-3">Checkout</a>
