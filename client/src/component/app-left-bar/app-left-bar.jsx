5c50c7b4b52f4912ad3e398d38143ef2161cc1c0
Santo Khan <santok54@gmail.com>
2021-05-26 18:04:42 +0600

    Create app-left-bar.jsx

---
+++ client/src/component/app-left-bar/app-left-bar.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './left-bar.css';
+
+function AppLeftBar() {
+    return (
+        <div className="app-container">
+            <div className="app-left-bar"></div>
+            <div className="app-middle"></div>
+            <div className="app-right-bar"></div>
+        </div>
+    );
+}
